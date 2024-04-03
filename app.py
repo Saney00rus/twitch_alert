abfc4a29737706b5cba6b13700a9102be1e6a798
Александр <saney00rus@gmail.com>
2024-04-03 15:20:28 +0300

    test ver 1

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 import datetime
-
+import sqlite3
 import requests
 from flask import Flask, request
-import re
 
 app = Flask(__name__)
 
-nicknames = ['anastazecs',]
+
+
+def get_users():
+
+    nicknames = []
+    # Подключение к базе данных (если базы данных не существует, она будет создана)
+    conn = sqlite3.connect('players.db')
+
+    # Создание курсора для выполнения SQL-запросов
+    cursor = conn.cursor()
+
+    # Создание таблицы players с тремя столбцами: id, name, elo
+    cursor.execute('''CREATE TABLE IF NOT EXISTS players (
+                        id TEXT PRIMARY KEY,
+                        name TEXT,
+                        elo INTEGER
+                    )''')
+
+    nicks = cursor.execute("SELECT name FROM players").fetchall()
+    for nick in nicks:
+        nicknames.append(nick[0])
+
+    conn.commit()
+    conn.close()
+    return nicknames
 
 
 @app.route('/receive_post', methods=['POST'])
@@ -14,6 +37,8 @@ def receive_post():
     if request.method == 'POST':
         data = request.get_json()
         print("\nПолучен POST запрос\n")
+        print(data)
+        print("\n\n")
         print(main(data))
 
         return "POST запрос успешно получен и обработан"
@@ -22,11 +47,12 @@ def receive_post():
 
 @app.route('/<nickname>', methods=['GET'])
 def show_stats(nickname):
-    if nickname in nicknames:
+    if nickname in get_users():
         res = get_info(nickname)
         return prnt_res(res)
     else:
         return "Нет данных для этого никнейма"
+
 
 def get_info(name):
     url = f"https://open.faceit.com/data/v4/players?nickname={name}"
@@ -40,11 +66,15 @@ def get_info(name):
         if response1.status_code == 200:
             data = response1.json()
             stats = data['items'][0]['stats']
+            kd = stats['K/D Ratio']
+            kr = stats['K/R Ratio']
+            kills = stats['Kills']
+            death = stats['Deaths']
             print("ELO:", elo)
-            print("K/D Ratio:", stats['K/D Ratio'])
-            print("K/R Ratio:", stats['K/R Ratio'])
-            print("Kills:", stats['Kills'])
-            print("Deaths:", stats['Deaths'])
+            print("K/D Ratio:", kd)
+            print("K/R Ratio:", kr)
+            print("Kills:", kills)
+            print("Deaths:", death)
 
 
 
@@ -75,7 +105,7 @@ def main(data):
     if data["event"] == "match_status_finished":
         for d in data["payload"]["teams"]:
             for a in d['roster']:
-                if a['nickname'] in nicknames:
+                if a['nickname'] in get_users():
                     res = get_info(a['nickname'])
                     # return prnt_res(res)
 
